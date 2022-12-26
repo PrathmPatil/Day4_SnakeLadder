38f5bda285729b7968e3ea7cf75f64bcca0a33f0
PrathmPatil <prathmpatil2818@gmail.com>
2022-12-27 01:39:10 +0530

    Please ignore me!

---
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,14 @@
+package com.snakeandladder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+           System.out.println("Welcome in SnakeAndLadder Game");
+           System.out.println();
+           int start=0;
+           int player1;
+           System.out.println("Start position of player is "+start);
+
+	}
+
+}
