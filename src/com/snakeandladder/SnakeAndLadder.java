65aaffdf67d7d3307d385b4e8496671958261083
PrathmPatil <prathmpatil2818@gmail.com>
2022-12-27 18:37:36 +0530

    Repeat till the Player
    reaches the winning
    
    position 100.

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -2,7 +2,7 @@ package com.snakeandladder;
 import java.util.Random;
 
 public class SnakeAndLadder {
-
+ static int dice;
 	public static void main(String[] args) {
            System.out.println("Welcome in SnakeAndLadder Game");
            System.out.println();
@@ -10,11 +10,11 @@ public class SnakeAndLadder {
            int player1=0;
            System.out.println("Start position of player is "+start);
            Random random= new Random ();
-        	   int dice=random.nextInt(5+1)+1; //((max-min)+1)+min;
-               System.out.println(dice);
                int checkPlay;
-               while(player1!=100) 
-               {
+               for(int i=0;i<3;i++) {
+               while(player1<100) 
+               { 
+            	   
             	   checkPlay=random.nextInt(2);
             	   if(checkPlay==0)
             	   {
@@ -25,34 +25,51 @@ public class SnakeAndLadder {
             	   }
             	   else
             	   {
+            		   dice=random.nextInt(5+1)+1; //((max-min)+1)+min;
             		   System.out.println("Play");
-            		   player1=player1+dice;
-            		   System.out.println(player1);
+            		   System.out.println(dice);
             		   
-            	   }
+            		   if(player1==7||player1==21||player1==28||player1==42||player1==63)
+                       {
+                    	   player1=player1+7;
+                       }
+                       else if(player1==12||player1==24||player1==48||player1==72||player1==86)
+                       {
+                    	   player1=player1+10;
+                       }
+                       else if(player1==25||player1==50||player1==75)
+                       {
+                    	   player1=player1+12;
+                       }
+                       else if(player1==19||player1==26||player1==36||player1==60||player1==84)
+                       {
+                    	   player1=player1-8;
+                       }
+                       else if(player1==23||player1==55||player1==44||player1==78||player1==96)
+                       {
+                    	   player1=player1-12;
+                       }
+                       else
+                       {    player1=player1+dice;
+                    	   if(player1>100)
+                    	   {
+                    		   player1=player1-dice;
+                    	   }
+                    	   else if(player1< 0)
+                    	   {
+                    		   player1=0;
+                    	   }
+                    	   
+                		   System.out.println(player1);
+                       }
+            	   }  
+                   
                }
-               if(player1==7|player1==21||player1==28||player1==42||player1==63)
-               {
-            	   player1=player1+dice;
-               }
-               else if(player1==12|player1==24||player1==48||player1==72||player1==86)
-               {
-            	   player1=player1+dice;
-               }
-               else if(player1==25|player1==50||player1==75)
-               {
-            	   player1=player1+dice;
-               }
-               else if(player1==19||player1==26||player1==36||player1==60||player1==84)
-               {
-            	   player1=player1-dice;
-               }
-               else if(player1==23||player1==55||player1==44||player1==78||player1==96)
-               {
-            	   player1=player1-dice;
-               }
+               System.out.println("end "+i);
+               player1=0;
+               System.out.println("****************************");
                
-               
+          }   
 	}
 
 }
